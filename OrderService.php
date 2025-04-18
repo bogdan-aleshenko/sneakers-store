3135f440d8243e8f3b3222ced5c282c23a531f0f
yunnas300 <yunnas300@gmail.com>
2025-04-18 16:25:04 +0200

    lab#5 all files

---
+++ OrderService.php
@@ -0,0 +1,17 @@
+/**
+* Сервіс для роботи з замовленнями.
+* Відповідає за бізнес-логіку замовлень.
+*/
+class OrderService {
+
+/**
+* Знаходить замовлення за ідентифікатором.
+*
+* @param int $id унікальний ідентифікатор замовлення
+* @return Order обʼєкт Order
+* @throws OrderNotFoundException якщо замовлення не знайдено
+*/
+public function getOrderById(int $id) {
+// Код
+}
+}
---
+++ ProductService.php
@@ -0,0 +1,25 @@
+/**
+* Сервіс для роботи з продуктами.
+* Забезпечує CRUD операції для товарів.
+*/
+class ProductService {
+
+/**
+* Повертає список всіх продуктів.
+*
+* @return Product[] список обʼєктів Product
+*/
+public function getAllProducts() {
+// Код
+}
+
+/**
+* Створює новий продукт.
+*
+* @param Product $product обʼєкт Product для створення
+* @return Product створений продукт
+*/
+public function createProduct(Product $product) {
+// Код
+}
+}
